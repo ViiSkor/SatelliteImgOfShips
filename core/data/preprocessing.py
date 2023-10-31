f64d7c2a048863aada7d0663b5fca9e99088d913
Вiкторiя Скорiк <vikaskorik@gmail.com>
2023-10-31 17:28:18 +0200

    Update preprocessing

--- core/data/preprocessing.py
+++ core/data/preprocessing.py
@@ -24,32 +24,47 @@ def get_train_val_sets(balanced_df: pd.DataFrame, masks: pd.DataFrame) -> Tuple[
     return train_df, valid_df
 
 
-def create_img_gen(
-        in_df: pd.DataFrame,
-        img_dir,
-        img_scaling: Optional[tuple[int, int]],
-        shuffle_batches: bool=True,
-        do_augmentation: bool=True,
-        transform: Optional[A.Compose]=None
-) -> Tuple[list, list]:
-    all_batches = list(in_df.groupby('ImageId'))
+class SemanticSegmentationDataGenerator(tf.keras.utils.Sequence):
+    def __init__(self, in_df, img_dir, batch_size, img_scaling=None, shuffle_batches=True, do_augmentation=False,
+                 transform=None):
+        self.in_df = in_df
+        self.img_dir = img_dir
+        self.batch_size = batch_size
+        self.img_scaling = img_scaling
+        self.shuffle_batches = shuffle_batches
+        self.do_augmentation = do_augmentation
+        self.transform = transform
 
-    while True:
-        if shuffle_batches:
-            np.random.shuffle(all_batches)
+        self.all_batches = list(self.in_df.groupby('ImageId'))
+        self.out_rgb, self.out_mask = [], []
 
-        for c_img_id, c_masks in all_batches:
-            rgb_path = os.path.join(img_dir, c_img_id)
+    def __len__(self):
+        return len(self.in_df) // self.batch_size
+
+    def __getitem__(self, index):
+        for c_img_id, c_masks in self.all_batches:
+            rgb_path = os.path.join(self.img_dir, c_img_id)
             c_img = imread(rgb_path)
             c_mask = np.expand_dims(masks_as_image(c_masks['EncodedPixels'].values), -1)
 
-            if img_scaling is not None:
-                c_img = c_img[::img_scaling[0], ::img_scaling[1]]
-                c_mask = c_mask[::img_scaling[0], ::img_scaling[1]]
+            if self.img_scaling is not None:
+                c_img = c_img[::self.img_scaling[0], ::self.img_scaling[1]]
+                c_mask = c_mask[::self.img_scaling[0], ::self.img_scaling[1]]
 
-            if do_augmentation:
-                transformed = transform(image=c_img, mask=c_mask)
+            if self.do_augmentation and self.transform is not None:
+                transformed = self.transform(image=c_img, mask=c_mask)
                 c_img = transformed['image']
                 c_mask = transformed['mask']
 
-            yield c_img / 255.0, c_mask
+            self.out_rgb.append(c_img)
+            self.out_mask.append(c_mask)
+
+            if len(self.out_rgb) >= self.batch_size:
+                batch_rgb = tf.convert_to_tensor(np.stack(self.out_rgb, 0) / 255.0, dtype=tf.float32)
+                batch_mask = tf.convert_to_tensor(np.stack(self.out_mask, 0), dtype=tf.float32)
+                self.out_rgb.clear(), self.out_mask.clear()
+                return batch_rgb, batch_mask
+
+        def on_epoch_end(self):
+            if self.shuffle_batches:
+                np.random.shuffle(self.all_batches)
